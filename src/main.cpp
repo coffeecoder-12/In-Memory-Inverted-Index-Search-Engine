e74f7c5aa5aaec2c4654a4c0396ae51d34abc1f5
Aditya Raj <rajadiofficial2@gmail.com>
2026-06-09 02:49:01 +0530

    Updated main.cpp included find_text_files function basic layout.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <filesystem>
+#include <string>
+#include <vector>
+
+std::vector<std::string> find_text_files(const std::string& dirPath){
+	std::vector<std::string> text_files;
+	try{
+		for(const auto& entry : std::filesystem::recursive_directory_iterator(dirPath)){
+
+		}
+	}catch(std::filesystem::filesystem_error& e){
+		std::cerr << "Error accessing directory " << dirPath << ": " << e.what() << std::endl;
+	}
+
+	return text_files;
+}
 
 int main() {
 	std::cout << "Holla, Search Engine!" << std::endl;
+	std::cout << find_text_files("./build");
 	return 0;
 }
